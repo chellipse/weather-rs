72f78361a94fcac0f0fc135472678bd0884d6a17
Chell L. <pumpkinbiscuit@protonmail.com>
2023-12-14 23:40:56 -0500

    man steps closer to -l

--- src/main.rs
+++ src/main.rs
@@ -1,132 +1,100 @@
 // rust weather script
-use serde::{Deserialize, Serialize};
-use reqwest::{Error,get};
-use std::collections::HashMap;
+use reqwest::Error;
 use std::env;
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Mutex;
+use lazy_static::lazy_static;
+use serde::de::DeserializeOwned;
 
-static QUIET: AtomicBool = AtomicBool::new(false);
+mod structs;
+use structs::{IpApiResponse, MeteoApiResponse};
 
+enum Modes {
+    Short,
+    Long,
+}
+
+lazy_static! {
+    static ref QUIET: Mutex<bool> = Mutex::new(false);
+    static ref MODE: Mutex<Modes> = Mutex::new(Modes::Short); // Default mode
+}
+
+// must be >= 1
 const PAST_DAYS: i32 = 1;
+// must be >= 2
 const FORECAST_DAYS: i32 = 2;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct IpApiResponse {
-    status: String,
-    lat: Option<f64>,
-    lon: Option<f64>,
-    timezone: Option<String>,
+const DEFAULT_LAT: f32 = 35.9145;
+const DEFAULT_LON: f32 = -78.9225;
+const DEFAULT_TIMEZONE: &str = "America/New_York";
+
+struct Rgb {
+    r: u8,
+    g: u8,
+    b: u8,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct MeteoApiResponse {
-    latitude: f64,
-    longitude: f64,
-    generationtime_ms: f64,
-    utc_offset_seconds: i64,
-    timezone: String,
-    timezone_abbreviation: String,
-    elevation: f64,
-    current_units: HashMap<String, String>,
-    current: CurrentData,
-    hourly_units: HashMap<String, String>,
-    hourly: HourlyData,
-    daily_units: HashMap<String, String>,
-    daily: DailyData,
+static WHITE: Rgb = Rgb { r: 222, g: 222, b: 222 };
+static BLACK: Rgb = Rgb { r: 0, g: 0, b: 0 };
+static RED: Rgb = Rgb { r: 255, g: 0, b: 0 };
+static GREEN: Rgb = Rgb { r: 0, g: 255, b: 0 };
+static BLUE: Rgb = Rgb { r: 0, g: 0, b: 255 };
+
+struct LerpVals {
+    val: Vec<f32>,
+    low: f32,
+    high: f32,
+    low_rgb: Rgb,
+    high_rgb: Rgb,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct CurrentData {
-    time: String,
-    interval: i32,
-    temperature_2m: f64,
-    relative_humidity_2m: i32,
-    weather_code: i32,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct HourlyData {
-    time: Vec<String>,
-    temperature_2m: Vec<f64>,
-    relative_humidity_2m: Vec<i32>,
-    dew_point_2m: Vec<f64>,
-    precipitation_probability: Vec<i32>,
-    weather_code: Vec<i32>,
-    wind_speed_10m: Vec<f64>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct DailyData {
-    time: Vec<String>,
-    temperature_2m_max: Vec<f64>,
-    temperature_2m_min: Vec<f64>,
-    sunrise: Vec<String>,
-    sunset: Vec<String>,
-    precipitation_probability_max: Vec<i32>,
-    wind_speed_10m_max: Vec<f64>,
-}
-
+// ...
 #[tokio::main]
-async fn request_ip_api(url: &str) -> Result<IpApiResponse, Error> {
-
-    if !QUIET.load(Ordering::SeqCst) {
-        println!("Getting ip-api response...");
+async fn request_api<T: DeserializeOwned>(url: &str) -> Result<T, Error> {
+    if !*QUIET.lock().unwrap() {
+        println!("Querying {}...", url.chars().skip(7).take(20).collect::<String>());
     }
 
-    let response = get(url).await?.json::<IpApiResponse>().await?;
+    let response = reqwest::get(url).await?.json::<T>().await?;
 
     Ok(response)
 }
 
-#[tokio::main]
-async fn request_meteo_api(url: &str) -> Result<MeteoApiResponse, Error> {
-
-    if !QUIET.load(Ordering::SeqCst) {
-    println!("Getting meteo-api response...");
-    }
-
-    let response = get(url).await?.json::<MeteoApiResponse>().await?;
-
-    Ok(response)
-}
-
+// ...
 fn make_meteo_url(ip_data: IpApiResponse) -> String {
-
-    let default_lat: f64 = 35.9145;
-    let default_lon: f64 = -78.9225;
-    let default_timezone = "America/New_York".to_string();
 
     let lat = match ip_data.lat {
         Some(value) => value,
         None => {
             println!("Using default lat...");
-            default_lat
+            DEFAULT_LAT
         },
     };
     let lon = match ip_data.lon {
         Some(value) => value,
         None => {
             println!("Using default lon...");
-            default_lon
+            DEFAULT_LON
         },
     };
     let timezone = match ip_data.timezone {
         Some(value) => value,
         None => {
             println!("Using default timezone...");
-            default_timezone
+            DEFAULT_TIMEZONE.to_string()
         },
     };
 
     let url = format!(
         concat!(
-            "https://api.open-meteo.com/v1/forecast?",
+            "http://api.open-meteo.com/v1/forecast?",
             "latitude={}&", // <--
             "longitude={}&", // <--
             "current=temperature_2m,relative_humidity_2m,weather_code&",
             "hourly=temperature_2m,relative_humidity_2m,dew_point_2m,precipitation_probability,weather_code,wind_speed_10m&",
+            "minutely_15=temperature_2m,relative_humidity_2m,dew_point_2m,precipitation_probability,weather_code,wind_speed_10m&",
             "daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_probability_max,wind_speed_10m_max&",
             "temperature_unit=fahrenheit&",
+            "timeformat=unixtime&",
             "timezone={}&", // <--
             "past_days={}&", // <--
             "forecast_days={}" // <--
@@ -136,6 +104,7 @@ fn make_meteo_url(ip_data: IpApiResponse) -> String {
     url
 }
 
+// ...
 fn wmo_decode<'a>(wmo: i32) -> &'a str {
     match wmo {
         0 => " ~Clear",
@@ -174,40 +143,132 @@ fn wmo_decode<'a>(wmo: i32) -> &'a str {
     }
 }
 
+// ...
+fn process_api_response<T>(input: Result<T, Error>, url: &str) -> T {
+    match input {
+        Ok(api_response) => api_response,
+        Err(e) => {
+            eprintln!("Request to \"{}\" failed with error: {}", url, e);
+            std::process::exit(1);
+        },
+    }
+}
+
+// outputs a String with color escapes equal to str + escapes for color rgb
+fn add_escapes(str: String, color: Rgb) -> String {
+    format!("\x1b[38;2;{};{};{}m{}\x1b[0m", color.r, color.g, color.b, str)
+}
+
+// linearly interpolates A's position between B and C to D and E
+fn lerp(a: f32, b: f32, c: f32, d: f32, e: f32) -> f32 {
+    (a - b) * (e - d) / (c - b) + d
+}
+
+// same as lerp() but the output values are Rgb structs
+fn rgb_lerp(x: f32, y: f32, z: f32, color1: &Rgb, color2: &Rgb) -> Rgb {
+    Rgb {
+        r: lerp(x, y, z, color1.r as f32, color2.r as f32) as u8,
+        g: lerp(x, y, z, color1.g as f32, color2.g as f32) as u8,
+        b: lerp(x, y, z, color1.b as f32, color2.b as f32) as u8,
+    }
+}
+
+// outputs a string with lerp'd color escapes
+// fn colorize_lerp(legend: &str, x: f32, y: f32, z: f32, color1: &Rgb, color2: &Rgb) -> String {
+//     let the_color = rgb_lerp(x, y, z, color1, color2);
+//     let display: String = format!(legend, x);
+//     add_escapes(display, the_color)
+// }
+
+// prints a single line weather update, good for status bars
+fn one_line_weather(md: MeteoApiResponse) {
+    let temp_now = md.current.temperature_2m;
+    let humid_now = md.current.relative_humidity_2m;
+    let precip_max = md.daily.precipitation_probability_max[PAST_DAYS as usize];
+    let wmo_msg_now = wmo_decode(md.current.weather_code);
+    println!("{}° {}% ~{}% {}", temp_now, humid_now, precip_max, wmo_msg_now);
+}
+
+// removes indices >
+fn rm_indices(input: Vec<f32>, current: u8, start: u8, end: u8) -> Vec<f32> {
+    let mut result = input;
+    result.drain(0 as usize..current as usize-start as usize);
+    result.truncate(end as usize+start as usize);
+    result
+}
+
+// ...
+fn long_weather(md: MeteoApiResponse) {
+
+    let time_data = md.minutely_15.time;
+    let start_time = 6 * 4;
+    let end_time = 24 * 4;
+    let mut current_time_index: u8 = 0 + start_time;
+
+    for (index, time) in time_data.iter().enumerate() {
+        if time == &md.current.time {
+            current_time_index = index as u8
+        }
+    };
+
+    let temp: Vec<f32> = rm_indices(md.minutely_15.temperature_2m.clone(), current_time_index, start_time, end_time);
+    // let temp_bar: Vec<&str> = mk_bar(temp);
+
+    let humid: Vec<f32> = rm_indices(md.minutely_15.relative_humidity_2m.clone(), current_time_index, start_time, end_time);
+
+    let precip: Vec<f32> = rm_indices(md.minutely_15.precipitation_probability.clone(), current_time_index, start_time, end_time);
+    // let precip_bar: Vec<&str> = mk_bar(precip);
+
+    // let wmo: Vec<&str> = get_wmo(md.minutely_15.weather_code);
+
+    for i in (0..temp.len()).step_by(4) {
+        // temp
+        let rgb_temp: Rgb = match temp[i] {
+            x if (0.0..100.0).contains(&x) => {
+                rgb_lerp(temp[i],0.0,100.0,&BLUE,&RED)
+            },
+            _ => {
+                rgb_lerp(temp[i],-100.0,130.0,&BLACK,&WHITE)
+            },
+        };
+        let format_temp = add_escapes(format!("{}°",temp[i]),rgb_temp);
+
+        // humidity
+        let rgb_humid = rgb_lerp(humid[i],0.0,100.0,&BLUE,&RED);
+        let format_humid = add_escapes(format!("{}%",humid[i]),rgb_humid);
+
+        // precipitation
+        let rgb_precip = rgb_lerp(precip[i],0.0,100.0,&WHITE,&BLUE);
+        let format_precip = add_escapes(format!("{}%",precip[i]),rgb_precip);
+
+        println!("{}   {}   {}", format_temp, format_humid, format_precip)
+    }
+}
+
 fn main() {
     for arg in env::args().skip(1) {
-        if arg == "--quiet" || arg == "-q" {
-            QUIET.store(true, Ordering::SeqCst);
+        match arg.as_str() {
+            "--quiet" | "-q" => *QUIET.lock().unwrap() = true,
+            "-l" => *MODE.lock().unwrap() = Modes::Long,
+            _ => {}
         }
     }
 
     let ip_url = "http://ip-api.com/json/";
-    let ip_response = request_ip_api(ip_url);
+    let ip_response: Result<IpApiResponse, Error> = request_api(ip_url);
+    let ip_data = process_api_response(ip_response, ip_url);
 
-    let ip_data = match ip_response {
-        Ok(api_response) => api_response,
-        Err(e) => {
-            eprintln!("Ip-api request failed with error: {}", e);
-            std::process::exit(1);
+    let meteo_url = &make_meteo_url(ip_data);
+    let meteo_response: Result<MeteoApiResponse, Error> = request_api(meteo_url);
+    let meteo_data = process_api_response(meteo_response, meteo_url);
+
+    match *MODE.lock().unwrap() {
+        Modes::Short => {
+            one_line_weather(meteo_data);
         },
-    };
-
-    let met_url = make_meteo_url(ip_data);
-    let meteo_response = request_meteo_api(&met_url);
-
-    let m_d = match meteo_response {
-        Ok(api_response) => api_response,
-        Err(e) => {
-            eprintln!("Meteo request failed with error: {}", e);
-            std::process::exit(1);
-        },
-    };
-
-    let temp = m_d.current.temperature_2m;
-    let humid = m_d.current.relative_humidity_2m;
-    let precip_max = m_d.daily.precipitation_probability_max[PAST_DAYS as usize];
-    let wmo_msg = wmo_decode(m_d.current.weather_code);
-    // println!("{:#?}",m_d);
-    println!("{}° {}% ~{}% {}", temp, humid, precip_max, wmo_msg)
+        Modes::Long => {
+            long_weather(meteo_data);
+        }
+    }
 }
 
---
+++ src/structs.rs
@@ -0,0 +1,70 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct IpApiResponse {
+    pub status: String,
+    pub lat: Option<f32>,
+    pub lon: Option<f32>,
+    pub timezone: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MeteoApiResponse {
+    pub latitude: f32,
+    pub longitude: f32,
+    pub generationtime_ms: f32,
+    pub utc_offset_seconds: i64,
+    pub timezone: String,
+    pub timezone_abbreviation: String,
+    pub elevation: f32,
+    pub current_units: HashMap<String, String>,
+    pub current: CurrentData,
+    pub hourly_units: HashMap<String, String>,
+    pub hourly: HourlyData,
+    pub minutely_15: FifteenMinutely,
+    pub daily_units: HashMap<String, String>,
+    pub daily: DailyData,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CurrentData {
+    pub time: u32,
+    pub interval: i32,
+    pub temperature_2m: f32,
+    pub relative_humidity_2m: i32,
+    pub weather_code: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct HourlyData {
+    pub time: Vec<u32>,
+    pub temperature_2m: Vec<f32>,
+    pub relative_humidity_2m: Vec<f32>,
+    pub dew_point_2m: Vec<f32>,
+    pub precipitation_probability: Vec<f32>,
+    pub weather_code: Vec<u8>,
+    pub wind_speed_10m: Vec<f32>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct FifteenMinutely {
+    pub time: Vec<u32>,
+    pub temperature_2m: Vec<f32>,
+    pub relative_humidity_2m: Vec<f32>,
+    pub dew_point_2m: Vec<f32>,
+    pub precipitation_probability: Vec<f32>,
+    pub weather_code: Vec<u8>,
+    pub wind_speed_10m: Vec<f32>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DailyData {
+    pub time: Vec<u32>,
+    pub temperature_2m_max: Vec<f32>,
+    pub temperature_2m_min: Vec<f32>,
+    pub sunrise: Vec<u32>,
+    pub sunset: Vec<u32>,
+    pub precipitation_probability_max: Vec<i32>,
+    pub wind_speed_10m_max: Vec<f32>,
+}
