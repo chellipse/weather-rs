7898e97acbcd657b0c4ba7a1568434a4f31747bf
Chell L. <pumpkinbiscuit@protonmail.com>
2023-12-21 18:37:16 -0500

    added --help and --version

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use std::time::{Instant, SystemTime, UNIX_EPOCH};
 use std::fs;
 use std::path::Path;
 use serde_json::Value;
+use std::process;
 
 mod structs;
 use structs::{IpApiResponse, MeteoApiResponse};
@@ -68,6 +69,20 @@ const DEFAULT_TIMEZONE: &str = "America/New_York";
 
 const BAR_MAX: usize = 18;
 
+const HELP_MSG: &str =
+"USAGE: weather [OPTIONS]
+  List weather information using Lat/Lon from ip-api.com with open-meteo.com
+
+OPTIONS
+      --help             Display this help message, then exit
+      --version          Display package name and version, then exit
+  -l, --long             Display hourly forecast
+  -q, --quiet            Disable non-Err messages
+      --no-color         Disable coler escapes
+  -f, --force-refresh    Disregard cache
+      --runtime-info     Display updates on program speed in ms
+";
+
 static WHITE: Rgb = Rgb { r: 222, g: 222, b: 222 };
 static BLACK: Rgb = Rgb { r: 0, g: 0, b: 0 };
 static L_GRAY: Rgb = Rgb { r: 180, g: 180, b: 180 };
@@ -80,6 +95,8 @@ static ICE_BLUE: Rgb = Rgb { r: 157, g: 235, b: 255 };
 static CLEAR_BLUE: Rgb = Rgb { r: 92, g: 119, b: 242 };
 static MID_BLUE: Rgb = Rgb { r: 68, g: 99, b: 240 };
 static DEEP_BLUE: Rgb = Rgb { r: 45, g: 80, b: 238 };
+
+static PURPLE: Rgb = Rgb { r: 58, g: 9, b: 66 };
 
 // ...
 #[tokio::main]
@@ -141,39 +158,39 @@ fn make_meteo_url(ip_data: IpApiResponse) -> String {
 // ...
 fn wmo_decode<'a>(wmo: u8) -> String {
     match wmo {
-        0 => add_esc(" ~Clear", &CLEAR_BLUE),
-        1 => add_esc(" <Clear", &CLEAR_BLUE),
-        2 => add_esc(" ~Cloudy", &L_GRAY),
-        3 => add_esc(" >Cloudy", &L_GRAY),
-        44|45 => add_esc(" ~Foggy", &L_GRAY),
-        48 => add_esc(" Fog+Rime", &L_GRAY),
-        51 => add_esc(" Drizzling-", &CLEAR_BLUE),
-        53 => add_esc(" Drizzling~", &MID_BLUE),
-        55 => add_esc(" Drizzling+", &DEEP_BLUE),
-        61 => add_esc(" Raining-", &CLEAR_BLUE),
-        63 => add_esc(" Raining~", &MID_BLUE),
-        65 => add_esc(" Raining+", &DEEP_BLUE),
-        71 => add_esc(" Snowing-", &CLEAR_BLUE),
-        73 => add_esc(" Snowing~", &CLEAR_BLUE),
-        75 => add_esc(" Snowing+", &CLEAR_BLUE),
-        77 => add_esc(" Snow Grains", &CLEAR_BLUE),
-        80 => add_esc(" Showers-", &CLEAR_BLUE),
-        81 => add_esc(" Showers~", &MID_BLUE),
-        82 => add_esc(" Showers+", &DEEP_BLUE),
-        85 => add_esc(" Snow Showers-", &CLEAR_BLUE),
-        86 => add_esc(" Snow Showers+", &CLEAR_BLUE),
-        95 => add_esc(" Thunderstorm~", &YELLOW),
-        0..=9 => add_esc("N/A 0-9", &CLEAR_BLUE),
-        10..=19 => add_esc("N/A 10-19", &CLEAR_BLUE),
-        20..=29 => add_esc("N/A 20-29", &CLEAR_BLUE),
-        30..=39 => add_esc("N/A 30-39", &CLEAR_BLUE),
-        40..=49 => add_esc("N/A 40-49", &CLEAR_BLUE),
-        50..=59 => add_esc("N/A 50-59", &CLEAR_BLUE),
-        60..=69 => add_esc("N/A 60-69", &CLEAR_BLUE),
-        70..=79 => add_esc("N/A 70-79", &CLEAR_BLUE),
-        80..=89 => add_esc("N/A 80-89", &CLEAR_BLUE),
-        90..=99 => add_esc("N/A 90-99", &CLEAR_BLUE),
-        _ => add_esc("N/A", &CLEAR_BLUE)
+        0 => add_fg_esc(" ~Clear", &CLEAR_BLUE),
+        1 => add_fg_esc(" <Clear", &CLEAR_BLUE),
+        2 => add_fg_esc(" ~Cloudy", &L_GRAY),
+        3 => add_fg_esc(" >Cloudy", &L_GRAY),
+        44|45 => add_fg_esc(" ~Foggy", &L_GRAY),
+        48 => add_fg_esc(" Fog+Rime", &L_GRAY),
+        51 => add_fg_esc(" Drizzling-", &CLEAR_BLUE),
+        53 => add_fg_esc(" Drizzling~", &MID_BLUE),
+        55 => add_fg_esc(" Drizzling+", &DEEP_BLUE),
+        61 => add_fg_esc(" Raining-", &CLEAR_BLUE),
+        63 => add_fg_esc(" Raining~", &MID_BLUE),
+        65 => add_fg_esc(" Raining+", &DEEP_BLUE),
+        71 => add_fg_esc(" Snowing-", &CLEAR_BLUE),
+        73 => add_fg_esc(" Snowing~", &CLEAR_BLUE),
+        75 => add_fg_esc(" Snowing+", &CLEAR_BLUE),
+        77 => add_fg_esc(" Snow Grains", &CLEAR_BLUE),
+        80 => add_fg_esc(" Showers-", &CLEAR_BLUE),
+        81 => add_fg_esc(" Showers~", &MID_BLUE),
+        82 => add_fg_esc(" Showers+", &DEEP_BLUE),
+        85 => add_fg_esc(" Snow Showers-", &CLEAR_BLUE),
+        86 => add_fg_esc(" Snow Showers+", &CLEAR_BLUE),
+        95 => add_fg_esc(" Thunderstorm~", &YELLOW),
+        0..=9 => add_fg_esc("N/A 0-9", &CLEAR_BLUE),
+        10..=19 => add_fg_esc("N/A 10-19", &CLEAR_BLUE),
+        20..=29 => add_fg_esc("N/A 20-29", &CLEAR_BLUE),
+        30..=39 => add_fg_esc("N/A 30-39", &CLEAR_BLUE),
+        40..=49 => add_fg_esc("N/A 40-49", &CLEAR_BLUE),
+        50..=59 => add_fg_esc("N/A 50-59", &CLEAR_BLUE),
+        60..=69 => add_fg_esc("N/A 60-69", &CLEAR_BLUE),
+        70..=79 => add_fg_esc("N/A 70-79", &CLEAR_BLUE),
+        80..=89 => add_fg_esc("N/A 80-89", &CLEAR_BLUE),
+        90..=99 => add_fg_esc("N/A 90-99", &CLEAR_BLUE),
+        _ => add_fg_esc("N/A", &CLEAR_BLUE)
     }
 }
 
@@ -188,9 +205,17 @@ fn process_api_response<T>(input: Result<T, Error>, url: &str) -> T {
     }
 }
 
-fn add_esc(str: &str, color: &Rgb) -> String {
+fn add_fg_esc(str: &str, color: &Rgb) -> String {
     if !SETTINGS.lock().unwrap().no_color {
         format!("\x1b[38;2;{};{};{}m{}", color.r, color.g, color.b, str)
+    } else {
+        str.to_string()
+    }
+}
+
+fn add_bg_esc(str: &str, color: &Rgb) -> String {
+    if !SETTINGS.lock().unwrap().no_color {
+        format!("\x1b[48;2;{};{};{}m{}", color.r, color.g, color.b, str)
     } else {
         str.to_string()
     }
@@ -251,13 +276,23 @@ fn mk_bar(val: &f32, low: &f32, high: &f32, bar_low: &f32, bar_max: usize) -> St
 }
 
 fn fill_right(msg: String, max: usize) -> String {
-    let remain: usize = max - msg.chars().count();
+    let mut remain: usize = 0;
+    if max >= msg.chars().count() {
+        remain = max - msg.chars().count();
+    } else {
+        print!("fill_right() Err: {}-{}", max, msg.chars().count())
+    }
     let spaces: String = " ".repeat(remain);
     format!("{}{}", msg, spaces)
 }
 
 fn fill_left(msg: String, max: usize) -> String {
-    let remain = max - msg.chars().count();
+    let mut remain: usize = 0;
+    if max >= msg.chars().count() {
+        remain = max - msg.chars().count();
+    } else {
+        print!("fill_left() Err: {}-{}", max, msg.chars().count())
+    }
     let spaces: String = " ".repeat(remain);
     format!("{}{}", spaces, msg)
 }
@@ -319,7 +354,7 @@ fn long_weather(md: MeteoApiResponse) {
     for i in (0..temp.len()).step_by(4) {
         // hour title
         if i as u8 == START_DISPLAY {
-            print!("\x1b[48;2;58;9;66;35;1mNow ");
+            print!("{}", add_bg_esc("Now ", &PURPLE));
         } else {
             print!("    ");
         };
@@ -329,7 +364,8 @@ fn long_weather(md: MeteoApiResponse) {
         let hour = (time_offset / 3600) % 24; // 3600 seconds in an hour
         let am_pm = to_am_pm(hour);
         let hour_stdwth = fill_left(am_pm.to_string(), 4);
-        let hour_format = add_esc(&hour_stdwth, &WHITE);
+        let hour_format = add_fg_esc(&hour_stdwth, &WHITE);
+        print!("{}  ", hour_format);
 
         // temp
         let rgb_temp: Rgb = match temp[i] {
@@ -352,7 +388,8 @@ fn long_weather(md: MeteoApiResponse) {
                 rgb_lerp(temp[i],-100.0,130.0,&BLACK,&WHITE)
             },
         };
-        let format_temp = add_esc(&format!("{:.1}°",temp[i]),&rgb_temp);
+        let format_temp = add_fg_esc(&format!("{:.1}°",temp[i]),&rgb_temp);
+        print!("{}  ", format_temp);
 
         // temp bar
         let mut low: f32 = *temp.iter().min_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
@@ -362,32 +399,30 @@ fn long_weather(md: MeteoApiResponse) {
             low = low - 5.0;
         }
         let temp_bar = mk_bar(&temp[i], &low, &high, &1.0, BAR_MAX);
-        let format_temp_bar = add_esc(&format!("{}",temp_bar),&rgb_temp);
+        let format_temp_bar = add_fg_esc(&format!("{}",temp_bar),&rgb_temp);
+        print!("{}  ", format_temp_bar);
 
         // humidity
         let rgb_humid = rgb_lerp(humid[i],30.0,90.0,&WHITE,&DEEP_BLUE);
         let humid_strwth = fill_left(format!("{}%",humid[i]), 4);
-        let format_humid = add_esc(&humid_strwth,&rgb_humid);
+        let format_humid = add_fg_esc(&humid_strwth,&rgb_humid);
+        print!("{}  ", format_humid);
 
         // precipitation
         let rgb_precip = rgb_lerp(precip[i],0.0,100.0,&ICE_BLUE,&DEEP_BLUE);
         let precip_strwth = fill_left(format!("{}%",precip[i]), 4);
-        let format_precip = add_esc(&precip_strwth,&rgb_precip);
+        let format_precip = add_fg_esc(&precip_strwth,&rgb_precip);
+        print!("{}  ", format_precip);
 
         // precip bar
         let precip_bar = mk_bar(&precip[i], &0.0, &100.0, &0.0, BAR_MAX);
-        let format_precip_bar = add_esc(&format!("{}",precip_bar),&rgb_precip);
+        let format_precip_bar = add_fg_esc(&format!("{}",precip_bar),&rgb_precip);
+        print!("{}  ", format_precip_bar);
 
         // wmo code msg
         let format_wmo = wmo_decode(wmo[i]);
-
-        print!("{}  ", hour_format);
-        print!("{}  ", format_temp);
-        print!("{}  ", format_temp_bar);
-        print!("{}  ", format_humid);
-        print!("{}  ", format_precip);
-        print!("{}  ", format_precip_bar);
         print!("{}  ", format_wmo);
+
         println!("\x1b[0m");
     };
     optional_runtime_update();
@@ -444,14 +479,39 @@ fn is_cache_recent<P: AsRef<Path>>(path: P) -> bool {
 }
 
 fn main() {
+    // set options in SETTINGS struct based on args
     for arg in env::args().skip(1) {
         match arg.as_str() {
-            "--quiet" | "-q" => SETTINGS.lock().unwrap().quiet = true,
-            "--long" | "-l" => SETTINGS.lock().unwrap().mode = Modes::Long,
-            "--force-refresh" | "-f" => SETTINGS.lock().unwrap().cache_override = true,
+            "--" => break,
+            "--version" => {
+                let pkg_name = env!("CARGO_PKG_NAME");
+                let version = env!("CARGO_PKG_VERSION");
+                println!("{}: {}", pkg_name, version);
+                process::exit(0);
+            },
+            "--help" => {
+                let pkg_name = env!("CARGO_PKG_NAME");
+                let version = env!("CARGO_PKG_VERSION");
+                print!("{}: {}\n{}", pkg_name, version, HELP_MSG);
+                process::exit(0);
+            },
+            "--quiet" => SETTINGS.lock().unwrap().quiet = true,
+            "--long" => SETTINGS.lock().unwrap().mode = Modes::Long,
+            "--force-refresh" => SETTINGS.lock().unwrap().cache_override = true,
             "--runtime-info" => SETTINGS.lock().unwrap().runtime_info = true,
             "--no-color" => SETTINGS.lock().unwrap().no_color = true,
-            _ => println!("Unrecognized option: {}", arg)
+            arg if arg.starts_with("--") => { println!("Unrecognized option: {}", arg); process::exit(0); }
+            arg if arg.starts_with("-") => {
+                for char in arg.chars().skip(1) {
+                    match char {
+                        'q' => SETTINGS.lock().unwrap().quiet = true,
+                        'l' => SETTINGS.lock().unwrap().mode = Modes::Long,
+                        'f' => SETTINGS.lock().unwrap().cache_override = true,
+                        _ => { println!("Unrecognized option: -{}", char); process::exit(0); }
+                    }
+                }
+            }
+            _ => { println!("Unrecognized option: {}", arg); process::exit(0); }
         }
     }
     let settings_clone = {
@@ -487,10 +547,14 @@ fn main() {
             let json = serde_json::to_string(&meteo_data).unwrap();
             match fs::write(&save_location, json) {
                 Ok(_) => {
-                    println!("Cache saved.");
+                    if !SETTINGS.lock().unwrap().quiet {
+                        println!("Cache saved.");
+                    }
                 },
                 Err(x) => {
-                    println!("Error saving saving cache: {}", x);
+                    if !SETTINGS.lock().unwrap().quiet {
+                        println!("Error saving saving cache: {}", x);
+                    }
                 }
             }
             meteo_data
